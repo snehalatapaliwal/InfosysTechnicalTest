fa6043aca3d242d071cb02790143f1119c862e94
Snehalata <snehlata.paliwal@gmail.com>
2020-06-28 14:05:37 +0530

    Add Alamofire framework pod

---
+++ InfosysTechnicalTest/Pods/Target Support Files/Pods-InfosysTechnicalTest-InfosysTechnicalTestUITests/Pods-InfosysTechnicalTest-InfosysTechnicalTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InfosysTechnicalTest_InfosysTechnicalTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_InfosysTechnicalTest_InfosysTechnicalTestUITests
+@end
